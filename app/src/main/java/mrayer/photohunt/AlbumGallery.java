290700f1afd0651a9ecda3f6449c9a199dfc4deb
rolledback <matthew.rayermann@gmail.com>
2016-03-08 14:30:15 -0600

    Made project.

---
+++ app/src/main/java/mrayer/photohunt/AlbumGallery.java
@@ -0,0 +1,13 @@
+package mrayer.photohunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlbumGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_gallery);
+    }
+}
