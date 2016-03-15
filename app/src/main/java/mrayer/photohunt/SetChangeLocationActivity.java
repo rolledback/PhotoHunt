d2a2442d8e6885935d0530eefc8716f411e73fee
Aila Enos <ailaenos@utexas.edu>
2016-03-15 16:14:19 -0500

    Changed SetChangeLocation Activity to a Google Maps Activity

--- app/src/main/java/mrayer/photohunt/SetChangeLocationActivity.java
+++ app/src/main/java/mrayer/photohunt/SetChangeLocationActivity.java
@@ -1,35 +1,85 @@
 package mrayer.photohunt;
 
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Toast;
 
-/* Aila's TODO
-Open up Google maps after selecting "View/Add Location" in Create New Photo Hunt activity
-Intent needs to send location coordinates to Google Maps
-Intent needs to return location coordinates to Create New Photo Hunt Activity
-*/
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SetChangeLocationActivity extends AppCompatActivity {
+/* Aila's TODO:
+Add key in when need to test
+Intent from "View/Add Location" button in Create New Photo Hunt activity that sends coords
+Need to get location user taps
+Intent needs to return location coordinates to Create New Photo Hunt activity - on button submit?
+ */
+
+public class SetChangeLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_change_location);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        // Ideally, the lat and long should be retrieved from the Intent bundle
+        // Markers received click events by default, set draggable property to true
+        // Need to retrieve marker's location on button click?
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onMapClick(LatLng latLng) {
+                Toast.makeText(SetChangeLocationActivity.this, "Position of click: " + latLng.toString(), Toast.LENGTH_LONG).show();
             }
         });
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                // Do nothing
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                // Do nothing
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker)
+            {
+                Toast.makeText(SetChangeLocationActivity.this, "Position: " + marker.getPosition(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
