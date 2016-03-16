b0a32f28dec77c4c1d383bc129e4855159aeaa86
chanjoongkim <chanjoong.cj.kim@gmail.com>
2016-03-15 23:45:13 -0500

    optimized image loading, should scroll much faster now

--- app/src/main/java/mrayer/photohunt/ImageAdapter.java
+++ app/src/main/java/mrayer/photohunt/ImageAdapter.java
@@ -5,6 +5,10 @@ package mrayer.photohunt;
  */
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -14,7 +18,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ImageAdapter extends PagerAdapter {
     Context context;
@@ -42,11 +50,19 @@ public class ImageAdapter extends PagerAdapter {
 
         String picturePath = galImages.get(position);
         File file = new File(picturePath);
-        Uri uri = Uri.fromFile(file);
-        imageView.setImageURI(uri);
+
         int height = (int) context.getResources().getDimension(R.dimen.create_new_photo_hunt_image_size);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(context.getResources().getDisplayMetrics().widthPixels, height);
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+
+        Bitmap bitmap = decodeFile(file, width, height);
+        imageView.setImageBitmap(bitmap);
+        Uri uri = Uri.fromFile(file);
+
+
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(width, height);
         imageView.setLayoutParams(params);
+
+//        imageView.setImageURI(uri);
 
         ((ViewPager) container).addView(imageView, 0);
         return imageView;
@@ -63,5 +79,67 @@ public class ImageAdapter extends PagerAdapter {
 
     public void setGalImages(ArrayList<String> newGalImages) {
         galImages = newGalImages;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    // Decodes image and scales it to reduce memory consumption
+    private Bitmap decodeFile(File f, int requiredWidth, int requiredHeight) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= requiredWidth &&
+                    o.outHeight / scale / 2 >= requiredHeight) {
+                scale *= 2;
+            }
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {}
+        return null;
     }
 }
