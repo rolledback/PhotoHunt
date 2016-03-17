9bc131dbe271f21c54e7becbceb138f1f18f0b89
Aila Enos <ailaenos@utexas.edu>
2016-03-16 20:53:43 -0500

    Clicking and dragging a marker work in SetChangeLocationActivity

--- app/src/main/java/mrayer/photohunt/SetChangeLocationActivity.java
+++ app/src/main/java/mrayer/photohunt/SetChangeLocationActivity.java
@@ -1,9 +1,17 @@
 package mrayer.photohunt;
 
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,19 +22,45 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 /* Aila's TODO:
 Add key in when need to test
-Intent from "View/Add Location" button in Create New Photo Hunt activity that sends coords
-Need to get location user taps
-Intent needs to return location coordinates to Create New Photo Hunt activity - on button submit?
+Intent from "View/Add Location" button in Create New Photo Hunt activity that sends LatLng
  */
 
-public class SetChangeLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+public class SetChangeLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker m;
+    private LatLng currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_change_location);
+
+        // Obtain the current location IF there is one from the Bundle
+        if(!(getIntent().getParcelableExtra("bundle") == null))
+        {
+            Bundle bundle = getIntent().getParcelableExtra("bundle");
+            LatLng defaultPos = bundle.getParcelable("location");
+            currentPos = defaultPos;
+        }
+        else
+        {
+            currentPos = new LatLng(-1, -1);
+        }
+
+        // If the button is clicked, start an intent that sends the location back to the Create New Photo Hunt Activity
+        Button b = (Button) findViewById(R.id.setLocation);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Intent intent = new Intent(SetChangeLocationActivity.this, CreateNewPhotoHuntActivity.class);
+                Bundle latLng = new Bundle();
+                latLng.putParcelable("location", currentPos);
+                intent.putExtra("bundle", latLng);
+                startActivity(intent);
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -47,18 +81,27 @@ public class SetChangeLocationActivity extends FragmentActivity implements OnMap
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        // Ideally, the lat and long should be retrieved from the Intent bundle
-        // Markers received click events by default, set draggable property to true
-        // Need to retrieve marker's location on button click?
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").draggable(true));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        // Set a mark at the user's current location
+//        if(currentPos.latitude == -1 && currentPos.longitude == -1)
+//        {
+//            LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+//            Criteria criteria = new Criteria();
+//            String provider = service.getBestProvider(criteria, false);
+//            Location location = service.getLastLocation(provider);
+//            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+//            currentPos = userLocation;
+//        }
 
+        m = mMap.addMarker(new MarkerOptions().position(currentPos).draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPos));
+
+        // Clicking on the map will set the current position to that
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                Toast.makeText(SetChangeLocationActivity.this, "Position of click: " + latLng.toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(SetChangeLocationActivity.this, "Setting currentPos to " + latLng.toString(), Toast.LENGTH_LONG).show();
+                m.setPosition(latLng);
+                currentPos = latLng;
             }
         });
 
@@ -76,7 +119,8 @@ public class SetChangeLocationActivity extends FragmentActivity implements OnMap
             @Override
             public void onMarkerDragEnd(Marker marker)
             {
-                Toast.makeText(SetChangeLocationActivity.this, "Position: " + marker.getPosition(), Toast.LENGTH_LONG).show();
+                Toast.makeText(SetChangeLocationActivity.this, "Setting currentPos to " + marker.getPosition(), Toast.LENGTH_LONG).show();
+                currentPos = marker.getPosition();
             }
         });
 
