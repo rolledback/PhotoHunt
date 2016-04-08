11bf99feda82f76b53f4011056d4cc06c974071a
rolledback <matthew.rayermann@gmail.com>
2016-04-08 02:28:57 -0500

    Cleaned up strings.xml and the manifest. Also added up button functionality for the two favorites activities.

--- app/src/main/java/mrayer/photohunt/FavoriteUsersActivity.java
+++ app/src/main/java/mrayer/photohunt/FavoriteUsersActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ListView;
@@ -37,4 +38,12 @@ public class FavoriteUsersActivity extends AppCompatActivity {
         favoriteUsersListView.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/mrayer/photohunt/FavoritedByActivity.java
+++ app/src/main/java/mrayer/photohunt/FavoritedByActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ListView;
@@ -35,6 +36,15 @@ public class FavoritedByActivity extends AppCompatActivity {
 
         favoritedByListView = (ListView) findViewById(R.id.favorited_by_list);
         favoritedByListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
